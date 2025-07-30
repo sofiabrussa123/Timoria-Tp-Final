9490b7c2816b932ca811fdf23d997c1070a7a0bf
Enzo Stigliano <131629043+EnzoStigliano@users.noreply.github.com>
2025-07-30 18:23:36 -0300

    Implementar inputProcessor

--- core/src/main/java/io/github/timoria/EscenaBase.java
+++ core/src/main/java/io/github/timoria/EscenaBase.java
@@ -5,9 +5,7 @@ import com.badlogic.gdx.Screen;
 import com.badlogic.gdx.graphics.GL20;
 import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
-import com.badlogic.gdx.math.Vector2;
-import com.badlogic.gdx.physics.box2d.Box2DDebugRenderer;
-import com.badlogic.gdx.physics.box2d.World;
+import com.badlogic.gdx.InputMultiplexer;
 import com.badlogic.gdx.scenes.scene2d.Stage;
 import com.badlogic.gdx.scenes.scene2d.ui.Skin;
 import com.badlogic.gdx.scenes.scene2d.ui.TextButton;
@@ -17,7 +15,7 @@ import com.badlogic.gdx.scenes.scene2d.InputEvent;
 import com.badlogic.gdx.Input;
 import com.badlogic.gdx.utils.viewport.ScreenViewport;
 
-public abstract class EscenaBase implements Screen{
+public abstract class EscenaBase implements Screen {
 
     protected Principal juego;
     protected Stage escena;
@@ -29,11 +27,11 @@ public abstract class EscenaBase implements Screen{
     protected Skin skinPausa;
 
     public EscenaBase(Principal juego, String fondo) {
-		this.juego = juego;
-		this.escena = new Stage(new ScreenViewport());
-		this.batch = new SpriteBatch();
-		this.fondo = new Texture(fondo);
-        skinPausa = new Skin(Gdx.files.internal("uiskin.json")); // Asegurate que tenés este archivo
+        this.juego = juego;
+        this.escena = new Stage(new ScreenViewport());
+        this.batch = new SpriteBatch();
+        this.fondo = new Texture(fondo);
+        skinPausa = new Skin(Gdx.files.internal("uiskin.json"));
 
         escenaPausa = new Stage(new ScreenViewport());
 
@@ -43,7 +41,7 @@ public abstract class EscenaBase implements Screen{
         btnSeguir.addListener(new ClickListener() {
             public void clicked(InputEvent event, float x, float y) {
                 juegoPausado = false;
-                Gdx.input.setInputProcessor(escena); // Volver al juego
+                Gdx.input.setInputProcessor(crearMultiplexer());
             }
         });
 
@@ -54,7 +52,6 @@ public abstract class EscenaBase implements Screen{
             }
         });
 
-
         Table tabla = new Table();
         tabla.setFillParent(true);
         tabla.center();
@@ -63,38 +60,22 @@ public abstract class EscenaBase implements Screen{
         tabla.add(btnMenu).pad(10);
 
         escenaPausa.addActor(tabla);
-
-	}
+    }
 
     @Override
     public void render(float delta) {
-        // Limpiar la pantalla
         Gdx.gl.glClearColor(0, 0, 0, 1);
         Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
 
-        // Dibujar el fondo
         batch.begin();
         batch.draw(fondo, 0, 0, escena.getViewport().getWorldWidth(), escena.getViewport().getWorldHeight());
         batch.end();
-
-        // Dibujar la escena
-        escena.act(delta);
-        escena.draw();
 
         if (Gdx.input.isKeyJustPressed(Input.Keys.ESCAPE) || Gdx.input.isKeyJustPressed(Input.Keys.P)) {
             juegoPausado = !juegoPausado;
-            Gdx.input.setInputProcessor(juegoPausado ? escenaPausa : escena);
+            Gdx.input.setInputProcessor(juegoPausado ? escenaPausa : crearMultiplexer());
         }
 
-        Gdx.gl.glClearColor(0, 0, 0, 1);
-        Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
-
-// Fondo
-        batch.begin();
-        batch.draw(fondo, 0, 0, escena.getViewport().getWorldWidth(), escena.getViewport().getWorldHeight());
-        batch.end();
-
-// Dibujo
         if (juegoPausado) {
             escenaPausa.act(delta);
             escenaPausa.draw();
@@ -102,13 +83,11 @@ public abstract class EscenaBase implements Screen{
             escena.act(delta);
             escena.draw();
         }
-
     }
 
     @Override
     public void resize(int width, int height) {
         escena.getViewport().update(width, height, true);
-        // Actualizar el batch para que coincida con el nuevo viewport
         batch.getProjectionMatrix().setToOrtho2D(0, 0, width, height);
     }
 
@@ -122,22 +101,23 @@ public abstract class EscenaBase implements Screen{
 
     @Override
     public void show() {
-        Gdx.input.setInputProcessor(escena); // para recibir input del mouse
+        Gdx.input.setInputProcessor(crearMultiplexer());
     }
 
     @Override
     public void resume() {
-        // No es necesario implementar
     }
 
     @Override
     public void pause() {
-        // No es necesario implementar
     }
 
     @Override
     public void hide() {
-        // No es necesario implementar
     }
 
-}
+    // 🔧 Este método puede ser sobreescrito en cada nivel
+    protected InputMultiplexer crearMultiplexer() {
+        return new InputMultiplexer(escena);
+    }
+} 
