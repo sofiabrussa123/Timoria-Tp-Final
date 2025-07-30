8d5f14eec3a08096e3973ae1469274646426d2f5
Silver_Rose <130399374+sofiabrussa123@users.noreply.github.com>
2025-07-30 08:15:12 -0300

    Arreglando problema de nombre en variables

--- core/src/main/java/io/github/timoria/Nivel1.java
+++ core/src/main/java/io/github/timoria/Nivel1.java
@@ -6,18 +6,18 @@ import com.badlogic.gdx.scenes.scene2d.ui.Skin;
 import personajes.Personaje;
 
 public class Nivel1 extends NivelBase {
-	
+
 	private Personaje jugador;
-	
+
 	public Nivel1(Principal juego, Skin skin) {
-		
+
 		super(juego, "FondoNivel1.jpeg");
-		
+
         // Crear el personaje usando el world del nivel base
-        jugador = new Personaje(world, "Jugador1", super.viewportWidth);
-        
+        jugador = new Personaje(mundo, "Jugador1", anchoViewport);
+
         // Añadir el personaje al stage (si NivelBase extiende de Stage)
         super.escena.addActor(jugador);
-		
+
 	}
 }
