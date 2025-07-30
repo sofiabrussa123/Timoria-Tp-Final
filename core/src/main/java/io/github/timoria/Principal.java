61419896140acad3a9835c4314b3de7104bf8325
Enzo Stigliano <131629043+EnzoStigliano@users.noreply.github.com>
2025-07-30 19:34:20 -0300

    Se añade constante de conversión de pixeles

--- core/src/main/java/io/github/timoria/Principal.java
+++ core/src/main/java/io/github/timoria/Principal.java
@@ -3,6 +3,7 @@ package io.github.timoria;
 import com.badlogic.gdx.Game;
 
 public class Principal extends Game {
+	public static final float PPM = 100;
     @Override
     public void create() {
         setScreen(new Menu(this));
