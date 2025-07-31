7be5bf3a3f6695937fa18b45cb21607f4339f183
Enzo Stigliano <131629043+EnzoStigliano@users.noreply.github.com>
2025-07-31 18:53:53 -0300

    Create Enemigo.java
    Se añade enemigo que baja la vida y sigue al jugador

---
+++ core/src/main/java/personajes/Enemigo.java
@@ -0,0 +1,84 @@
+package personajes;
+
+import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.graphics.g2d.Batch;
+import com.badlogic.gdx.math.Vector2;
+import com.badlogic.gdx.physics.box2d.*;
+import com.badlogic.gdx.scenes.scene2d.Actor;
+
+import io.github.timoria.NivelBase;
+
+public class Enemigo extends Actor {
+
+    private Texture textura;
+    private Body cuerpo;
+    private float ancho;
+    private float alto;
+    private float daño;
+
+    private Personaje objetivo;
+
+    public Enemigo(World mundo, float x, float y, float daño, Personaje jugador) {
+        this.textura = new Texture("enemigo.png"); // asegurate de tener esta imagen
+        this.objetivo = jugador;
+        this.daño = daño;
+
+        this.ancho = 64;
+        this.alto = 64;
+
+        BodyDef def = new BodyDef();
+        def.type = BodyDef.BodyType.DynamicBody;
+        def.position.set(x * NivelBase.PIXELES_A_METROS, y * NivelBase.PIXELES_A_METROS);
+        def.fixedRotation = true;
+
+        this.cuerpo = mundo.createBody(def);
+
+        PolygonShape forma = new PolygonShape();
+        forma.setAsBox(ancho / 2 * NivelBase.PIXELES_A_METROS, alto / 2 * NivelBase.PIXELES_A_METROS);
+
+        FixtureDef fixture = new FixtureDef();
+        fixture.shape = forma;
+        fixture.density = 1f;
+        fixture.friction = 0.5f;
+        cuerpo.createFixture(fixture);
+        forma.dispose();
+
+        setSize(ancho, alto);
+        cuerpo.setUserData(this);
+    }
+
+    public void aplicarDañoJugador(Personaje jugador) {
+        jugador.recibirDaño((int) daño); // o una constante como 10
+    }
+
+    @Override
+    public void act(float delta) {
+        super.act(delta);
+
+        // Movimiento hacia el jugador
+        Vector2 posicionJugador = objetivo.getCuerpo().getPosition();
+        Vector2 posicionEnemigo = cuerpo.getPosition();
+
+        Vector2 direccion = posicionJugador.cpy().sub(posicionEnemigo).nor().scl(1.5f);
+        cuerpo.setLinearVelocity(direccion.x, cuerpo.getLinearVelocity().y);
+
+        // Actualizar posición del actor
+        setPosition(
+            cuerpo.getPosition().x / NivelBase.PIXELES_A_METROS - ancho / 2,
+            cuerpo.getPosition().y / NivelBase.PIXELES_A_METROS - alto / 2
+        );
+    }
+
+    @Override
+    public void draw(Batch batch, float parentAlpha) {
+        batch.draw(textura, getX(), getY(), getWidth(), getHeight());
+    }
+
+    public float getDaño() {
+        return daño;
+    }
+
+    public void dispose() {
+        textura.dispose();
+    }
+}
