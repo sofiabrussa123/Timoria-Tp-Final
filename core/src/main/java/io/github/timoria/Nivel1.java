1d37a9d673fcf8f9a8021b66b9b65d14077acdad
Silver_Rose <130399374+sofiabrussa123@users.noreply.github.com>
2025-08-02 09:49:31 -0300

    Update Nivel1.java
    Se agregan plataformas

--- core/src/main/java/io/github/timoria/Nivel1.java
+++ core/src/main/java/io/github/timoria/Nivel1.java
@@ -1,39 +1,51 @@
 package io.github.timoria;
 
 import com.badlogic.gdx.InputMultiplexer;
-import com.badlogic.gdx.scenes.scene2d.ui.Skin;
-
 import entorno.Plataforma;
 import entorno.PuertaLlegada;
 import personajes.Enemigo;
 import personajes.InputPersonaje;
 import personajes.Personaje;
 
-
 public class Nivel1 extends NivelBase {
 
-	private Personaje jugador;
+    private Personaje jugador;
 
-	public Nivel1(Principal juego, Skin skin) {
-		super(juego, "FondoNivel1.jpeg");
+    public Nivel1(Principal juego) {
+        super(juego, "FondoNivel1.jpeg");
 
-		jugador = new Personaje(mundo, "Jugador1", 100, 85); // posición en píxeles
-		super.escena.addActor(jugador);
-		Plataforma plataforma = new Plataforma(mundo, 200, 130, 150, 20);
-		Plataforma piso = new Plataforma(mundo, 0, 0, 800, 50);
-		PuertaLlegada puerta = new PuertaLlegada(mundo, 600, 50, 40, 70);
-		Enemigo enemigo1 = new Enemigo(mundo, 400, 150, 10, jugador);
-		escena.addActor(enemigo1);
-		escena.addActor(puerta);
-		super.escena.addActor(plataforma);
-		super.escena.addActor(piso);
-	}
-	
-	@Override
-	protected InputMultiplexer crearMultiplexer() {
-	    InputMultiplexer multiplexer = new InputMultiplexer();
-	    multiplexer.addProcessor(new InputPersonaje(jugador));
-	    multiplexer.addProcessor(escena);
-	    return multiplexer;
-	}
+
+        // Crear jugador
+        jugador = new Personaje(mundo, "Jugador1", 100, 85, juego);
+        escena.addActor(jugador);
+
+        // Crear plataformas
+        Plataforma plataforma1 = new Plataforma(mundo, 200, 130, 150, 20);
+        Plataforma plataforma2 = new Plataforma(mundo, 495, 200, 95, 20);
+        Plataforma plataforma3 = new Plataforma(mundo, 80, 250, 95, 20);
+        Plataforma piso = new Plataforma(mundo, 0, 0, 800, 50);
+
+        // Crear puerta bloqueada
+        PuertaLlegada puerta = new PuertaLlegada(mundo, 600, 50, 40, 70);
+        escena.addActor(puerta);
+
+
+        // Crear enemigo
+        Enemigo enemigo1 = new Enemigo(mundo, 400, 150, 10, jugador);
+        escena.addActor(enemigo1);
+
+        // Agregar plataformas
+        escena.addActor(plataforma1);
+        escena.addActor(plataforma2);
+        escena.addActor(plataforma3);
+        escena.addActor(piso);
+    }
+
+    @Override
+    protected InputMultiplexer crearMultiplexer() {
+        InputMultiplexer multiplexer = new InputMultiplexer();
+        multiplexer.addProcessor(new InputPersonaje(jugador));
+        multiplexer.addProcessor(escena);
+        return multiplexer;
+    }
 }
