9b99e785aef1454e176d4ccdfea88af1e4b1df1f
Enzo Stigliano <131629043+EnzoStigliano@users.noreply.github.com>
2025-09-05 09:56:39 -0300

    Actualizo PantallaDeMuerte.java respetando la nueva estructura de carpetas

--- core/src/main/java/interfaces/PantallaDeMuerte.java
+++ core/src/main/java/interfaces/PantallaDeMuerte.java
@@ -1,4 +1,4 @@
-package menus;
+package interfaces;
 
 import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.Screen;
@@ -13,7 +13,7 @@ import com.badlogic.gdx.scenes.scene2d.utils.ClickListener;
 import com.badlogic.gdx.utils.Align;
 import com.badlogic.gdx.utils.viewport.ScreenViewport;
 import io.github.timoria.Principal;
-import io.github.timoria.Nivel1;
+import niveles.Nivel1;
 
 import java.util.Random;
 
