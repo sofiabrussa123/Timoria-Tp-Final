081d285e0ae3ae12532613a210aaab129303e0f1
Enzo Stigliano <131629043+EnzoStigliano@users.noreply.github.com>
2025-09-05 10:02:58 -0300

    Actualizo EscenaBase.java respetando la nueva estructura de carpetas

--- core/src/main/java/niveles/EscenaBase.java
+++ core/src/main/java/niveles/EscenaBase.java
@@ -1,14 +1,16 @@
-package io.github.timoria;
+package niveles;
 
 import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.InputMultiplexer;
 import com.badlogic.gdx.Screen;
 import com.badlogic.gdx.graphics.GL20;
 import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
-import com.badlogic.gdx.InputMultiplexer;
 import com.badlogic.gdx.scenes.scene2d.Stage;
 import com.badlogic.gdx.scenes.scene2d.ui.Skin;
 import com.badlogic.gdx.utils.viewport.ScreenViewport;
+
+import io.github.timoria.Principal;
 
 public abstract class EscenaBase implements Screen {
 
