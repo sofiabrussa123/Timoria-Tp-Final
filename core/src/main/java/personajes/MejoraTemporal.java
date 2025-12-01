5cfba8d2d3078d0534fc8a14e8b82db695da25c5
Silver_Rose <130399374+sofiabrussa123@users.noreply.github.com>
2025-12-01 16:38:55 -0300

    Create MejoraTemporal.java
    se agrega la clase encargada de las mejoras de los personajes

---
+++ core/src/main/java/personajes/MejoraTemporal.java
@@ -0,0 +1,66 @@
+package personajes;
+
+public class MejoraTemporal {
+
+    private static final int MAX_MEJORAS = 3;
+    private static final float BONUS_POR_MEJORA = 2f;
+
+    private int mejorasVida;
+    private int mejorasVelocidad;
+    private int mejorasSalto;
+
+    public MejoraTemporal() {
+        mejorasVida = 0;
+        mejorasVelocidad = 0;
+        mejorasSalto = 0;
+    }
+
+    public boolean mejorarVida() {
+        if (mejorasVida < MAX_MEJORAS) {
+            mejorasVida++;
+            return true;
+        }
+        return false;
+    }
+
+    public boolean mejorarVelocidad() {
+        if (mejorasVelocidad < MAX_MEJORAS) {
+            mejorasVelocidad++;
+            return true;
+        }
+        return false;
+    }
+
+    public boolean mejorarSalto() {
+        if (mejorasSalto < MAX_MEJORAS) {
+            mejorasSalto++;
+            return true;
+        }
+        return false;
+    }
+
+    public float getBonusVida() {
+        return mejorasVida * BONUS_POR_MEJORA;
+    }
+
+    public float getBonusVelocidad() {
+        return mejorasVelocidad * BONUS_POR_MEJORA;
+    }
+
+    public float getBonusSalto() {
+        return mejorasSalto * BONUS_POR_MEJORA;
+    }
+
+    // Para la UI
+    public int getMejorasVida() { return mejorasVida; }
+    public int getMejorasVelocidad() { return mejorasVelocidad; }
+    public int getMejorasSalto() { return mejorasSalto; }
+
+    public int getMaxMejoras() { return MAX_MEJORAS; }
+
+    public void reset() {
+        mejorasVida = 0;
+        mejorasVelocidad = 0;
+        mejorasSalto = 0;
+    }
+}
