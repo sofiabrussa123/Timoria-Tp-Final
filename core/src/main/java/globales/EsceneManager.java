073dc775fd9d39a1f7bd28c531cc87b395cdba3c
Silver_Rose <130399374+sofiabrussa123@users.noreply.github.com>
2025-12-10 20:00:31 -0300

    Update EsceneManager.java

--- core/src/main/java/globales/EsceneManager.java
+++ core/src/main/java/globales/EsceneManager.java
@@ -3,14 +3,13 @@ package globales;
 import niveles.EscenaBase;
 
 public abstract class EsceneManager {
-	
 	private static EscenaBase escenaActual;
-	
+
 	public static void setEscenaActual(EscenaBase escena) {
-		escenaActual = escena;
+        escenaActual = escena;
 	}
-	
+
 	public static EscenaBase getEscenaActual() {
-		return escenaActual;
+        return escenaActual;
 	}
 }
