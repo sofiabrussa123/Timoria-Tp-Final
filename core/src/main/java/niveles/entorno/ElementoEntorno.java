0d567e6e6bc9dccd799f7b9b5ebbefca28123ba2
Silver_Rose <130399374+sofiabrussa123@users.noreply.github.com>
2025-12-10 20:32:05 -0300

    Update ElementoEntorno.java

--- core/src/main/java/niveles/entorno/ElementoEntorno.java
+++ core/src/main/java/niveles/entorno/ElementoEntorno.java
@@ -11,8 +11,8 @@ import com.badlogic.gdx.physics.box2d.World;
 import com.badlogic.gdx.scenes.scene2d.Actor;
 
 import io.github.timoria.Principal;
-import niveles.NivelBase;
 
+// Clase base abstracta para todos los elementos del entorno del juego
 public abstract class ElementoEntorno extends Actor {
 
     protected World mundo;
@@ -26,59 +26,70 @@ public abstract class ElementoEntorno extends Actor {
     protected FixtureDef fixtureDef = new FixtureDef();
     protected final int ID;
 
+    // Constructor con dimensiones e ID
     protected ElementoEntorno(World mundo, float x, float y, float ancho, float alto, int id) {
-    	this.ID = id;
+        this.ID = id;
         this.mundo = mundo;
         this.x = x;
         this.y = y;
         this.ancho = ancho;
         this.alto = alto;
     }
-    
+
+    // Constructor sin dimensiones pero con ID
     protected ElementoEntorno(World mundo, float x, float y, int id) {
-    	this.ID = id;
+        this.ID = id;
         this.mundo = mundo;
         this.x = x;
         this.y = y;
     }
-    
+
+    // Crea y posiciona el cuerpo físico del elemento en el mundo Box2D
     protected void crearYPosicionarCuerpo() {
-    	// Definir el cuerpo estático
+        // Definir el cuerpo
         BodyDef bodyDef = new BodyDef();
-        bodyDef.position.set((x + this.ancho / 2) / Principal.PPM, (y + this.alto / 2) / Principal.PPM);
-        bodyDef.type = this.tipoCuerpo;
+        bodyDef.position.set(
+            (x + ancho / 2) / Principal.PPM,
+            (y + alto / 2) / Principal.PPM
+        );
+        bodyDef.type = tipoCuerpo;
+
         cuerpo = mundo.createBody(bodyDef);
         cuerpo.setUserData(this);
 
         // Definir la forma
         PolygonShape forma = new PolygonShape();
         forma.setAsBox(
-        	ancho / 2 / Principal.PPM,
-        	(alto / 2 - 2) / Principal.PPM
+            ancho / 2 / Principal.PPM,
+            (alto / 2 - 2) / Principal.PPM
         );
 
         // Definir la fixture
         fixtureDef.shape = forma;
         fixtureDef.friction = 0.5f;
-        this.cuerpo.createFixture(fixtureDef);
+        cuerpo.createFixture(fixtureDef);
         forma.dispose();
 
-        setBounds(x, y, this.ancho, this.alto);
+        setBounds(x, y, ancho, alto);
         cuerpo.setUserData(this);
     }
-    
+
     public Body getCuerpo() {
-    	return this.cuerpo;
+        return cuerpo;
     }
-    
+
+    public int getID() {
+        return ID;
+    }
+
     protected void setTipoCuerpo(BodyDef.BodyType tipo) {
         this.tipoCuerpo = tipo;
     }
-    
+
     protected void setFixtureDef(FixtureDef fixtureDef) {
-    	this.fixtureDef = fixtureDef;
+        this.fixtureDef = fixtureDef;
     }
-    
+
     @Override
     public void draw(Batch batch, float parentAlpha) {
         batch.draw(textura, getX(), getY(), getWidth(), getHeight());
@@ -87,7 +98,10 @@ public abstract class ElementoEntorno extends Actor {
     @Override
     public void act(float delta) {
         Vector2 posicion = cuerpo.getPosition();
-        setPosition(posicion.x * Principal.PPM - this.ancho / 2, posicion.y * Principal.PPM - this.alto / 2);
+        setPosition(
+            posicion.x * Principal.PPM - ancho / 2,
+            posicion.y * Principal.PPM - alto / 2
+        );
     }
 
     public void dispose() {
