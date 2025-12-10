c444c117b382c3278eda996bf62595c7265217e1
Silver_Rose <130399374+sofiabrussa123@users.noreply.github.com>
2025-12-10 20:21:49 -0300

    Update EscenaBase.java

--- core/src/main/java/niveles/EscenaBase.java
+++ core/src/main/java/niveles/EscenaBase.java
@@ -10,9 +10,12 @@ import com.badlogic.gdx.scenes.scene2d.ui.Image;
 import com.badlogic.gdx.scenes.scene2d.ui.Skin;
 import com.badlogic.gdx.utils.viewport.ScreenViewport;
 
-import Red.HiloServidor;
 import globales.InputManager;
 
+/**
+ * Clase base para todas las escenas del juego.
+ * Implementa Screen de libGDX y proporciona funcionalidad común.
+ */
 public abstract class EscenaBase implements Screen {
 
     protected Game juego;
@@ -22,9 +25,12 @@ public abstract class EscenaBase implements Screen {
     protected InputManager inputManager;
     protected Image imagenFondo;
 
-    //Constructor, inicializar variables
+    /**
+     * Constructor de la escena base.
+     * @param juego Instancia del juego principal
+     * @param fondo Ruta del archivo de textura para el fondo
+     */
     public EscenaBase(Game juego, String fondo) {
-    	
         this.juego = juego;
         this.escena = new Stage(new ScreenViewport());
         this.fondo = new Texture(fondo);
@@ -33,14 +39,20 @@ public abstract class EscenaBase implements Screen {
         this.imagenFondo = new Image(this.fondo);
         this.imagenFondo.setPosition(0, 0);
         this.imagenFondo.setSize(escena.getViewport().getWorldWidth(), escena.getViewport().getWorldHeight());
-        
-        escena.addActor(imagenFondo);    
-       }
+        escena.addActor(imagenFondo);
+    }
+
+    /**
+     * Cambia la escena actual a una nueva escena.
+     * @param nuevaEscena La nueva escena a mostrar
+     */
+    protected void cambiarEscena(EscenaBase nuevaEscena) {
+        this.juego.setScreen(nuevaEscena);
+    }
 
     @Override
     public void render(float delta) {
-    	
-    	//Limpiar frame anterior, rellenar con negro
+        // Limpiar frame anterior, rellenar con negro
         Gdx.gl.glClearColor(0, 0, 0, 1);
         Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
         escena.act(delta);
@@ -49,26 +61,26 @@ public abstract class EscenaBase implements Screen {
 
     @Override
     public void resize(int width, int height) {
-    	
         this.escena.getViewport().update(width, height, true);
     }
 
     @Override
     public void dispose() {
-    	
         this.escena.dispose();
-        if (this.fuenteTextos != null) this.fuenteTextos.dispose();
+        if (this.fuenteTextos != null) {
+            this.fuenteTextos.dispose();
+        }
     }
-    
-    //Establecer una nueva pantalla y asignar el stage de la nueva escena al InputProcessor
-    protected void cambiarEscena(EscenaBase nuevaEscena) {
-    	
-    	this.juego.setScreen(nuevaEscena);
-    }
-    
+
     @Override
-    public void show() {}
-    public void resume() {}
-    public void pause() {}
-    public void hide() {}
-} 
+    public void show() { }
+
+    @Override
+    public void resume() { }
+
+    @Override
+    public void pause() { }
+
+    @Override
+    public void hide() { }
+}
