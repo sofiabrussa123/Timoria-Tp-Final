8c6869bce608b65c35dd34b214e5d3a8e241ffdb
Silver_Rose <130399374+sofiabrussa123@users.noreply.github.com>
2025-12-10 20:35:09 -0300

    Update Plataforma.java

--- core/src/main/java/niveles/entorno/Plataforma.java
+++ core/src/main/java/niveles/entorno/Plataforma.java
@@ -4,13 +4,12 @@ import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.physics.box2d.World;
 
+// Plataforma estática en la que los jugadores pueden pararse
 public class Plataforma extends ElementoEntorno {
 
     public Plataforma(World mundo, float x, float y, int ancho, int alto, int id) {
-    	super(mundo, x, y, ancho, alto, id);
-
+        super(mundo, x, y, ancho, alto, id);
         textura = new Texture(Gdx.files.internal("plataforma.jpg"));
-
         super.crearYPosicionarCuerpo();
     }
 }
