607ae9c2ce105859761cf644cf4ff6baabc6a2bc
Silver_Rose <130399374+sofiabrussa123@users.noreply.github.com>
2025-12-10 20:36:48 -0300

    Update PuertaLlegada.java

--- core/src/main/java/niveles/entorno/PuertaLlegada.java
+++ core/src/main/java/niveles/entorno/PuertaLlegada.java
@@ -6,24 +6,35 @@ import com.badlogic.gdx.physics.box2d.World;
 
 import Red.HiloServidor;
 
+// Puerta de salida del nivel que puede ser desbloqueada con una llave
 public class PuertaLlegada extends ElementoEntorno {
-	
-	private boolean estaBloqueada = true;
+
+    private boolean estaBloqueada = true;
+    private HiloServidor hiloServidor;
 
     public PuertaLlegada(World mundo, float x, float y, int id) {
-    	super(mundo, x, y, 50, 95, id);
-        textura = new Texture(Gdx.files.internal("puerta.png")); 
-
+        super(mundo, x, y, 50, 95, id);
+        textura = new Texture(Gdx.files.internal("puerta.png"));
         super.crearYPosicionarCuerpo();
+    }
+
+    // Setter para el servidor (se debe llamar después de la construcción)
+    public void setHiloServidor(HiloServidor hiloServidor) {
+        this.hiloServidor = hiloServidor;
     }
 
     public boolean sePuedeCruzar() {
         return !estaBloqueada;
     }
 
-    public void desbloquear(HiloServidor hiloServidor) {
+    // Desbloquea la puerta y cambia su apariencia visual
+    public void desbloquear() {
         estaBloqueada = false;
         textura = new Texture(Gdx.files.internal("puertaAbierta.png"));
-        hiloServidor.enviarMensajeATodos("DesbloquearPuerta:"+this.ID);
+        
+        // Sincronizar desbloqueo en red si el servidor está disponible
+        if (hiloServidor != null) {
+            hiloServidor.enviarMensajeATodos("DesbloquearPuerta:" + this.ID);
+        }
     }
 }
