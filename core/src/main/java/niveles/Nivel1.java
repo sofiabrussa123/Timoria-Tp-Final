567d3095263a9718d73a9ca55f6b4290de3b6654
Silver_Rose <130399374+sofiabrussa123@users.noreply.github.com>
2025-12-10 20:19:56 -0300

    Update Nivel1.java

--- core/src/main/java/niveles/Nivel1.java
+++ core/src/main/java/niveles/Nivel1.java
@@ -1,7 +1,6 @@
 package niveles;
 
 import com.badlogic.gdx.Game;
-import com.badlogic.gdx.scenes.scene2d.Actor;
 
 import niveles.entorno.LlaveActivadora;
 import niveles.entorno.Palanca;
@@ -13,20 +12,22 @@ import personajes.Jugador;
 import personajes.accesorios.MejoraTemporal;
 
 public class Nivel1 extends NivelBase {
-	
-	private int cantActores = 0;
+    
+    private int cantActores = 0;
 
+    // Constructor sin mejoras (para primera partida)
     public Nivel1(Game juego) {
-
         this(juego, null, null);
-
     }
 
-    // Constructor con mejoras
+    // Constructor con mejoras (para reinicios después de muerte)
     public Nivel1(Game juego, MejoraTemporal mejorasJ1, MejoraTemporal mejorasJ2) {
-
         super(juego, "FondoNivel1.jpeg");
-        // Crear jugadores con ID (añadir el parámetro 1 y 2 al final)
+
+        // Configurar friendly fire
+        this.setFriendlyFire(true);
+
+        // Crear jugadores con ID
         super.jugador1 = new Jugador(mundo, "Jugador1", 100, 85, 1);
         super.jugador2 = new Jugador(mundo, "Jugador2", 120, 85, 2);
 
@@ -34,32 +35,38 @@ public class Nivel1 extends NivelBase {
         if (mejorasJ1 != null) {
             copiarMejoras(mejorasJ1, super.jugador1.getMejoras());
             super.jugador1.actualizarVidaConMejoras();
+        } else {
+            // Si no hay mejoras pasadas, usar las estáticas de NivelBase
+            copiarMejoras(NivelBase.getMejorasJugador1(), super.jugador1.getMejoras());
+            super.jugador1.actualizarVidaConMejoras();
         }
 
         if (mejorasJ2 != null) {
             copiarMejoras(mejorasJ2, super.jugador2.getMejoras());
             super.jugador2.actualizarVidaConMejoras();
+        } else {
+            // Si no hay mejoras pasadas, usar las estáticas de NivelBase
+            copiarMejoras(NivelBase.getMejorasJugador2(), super.jugador2.getMejoras());
+            super.jugador2.actualizarVidaConMejoras();
         }
 
         // Crear plataformas
-        //Plataforma plataforma1 = new Plataforma(mundo, 200, 130, 150, 20);
         Plataforma plataforma2 = new Plataforma(mundo, 495, 200, 75, 20, asignarIDActor());
         Plataforma plataforma3 = new Plataforma(mundo, 80, 250, 95, 20, asignarIDActor());
-        Plataforma piso = new Plataforma(mundo, 0, 10, 800, 50, asignarIDActor());
+        Plataforma piso = new Plataforma(mundo, 0, 10, 1000, 50, asignarIDActor());
 
+        // Crear palanca y plataforma móvil
         Palanca palanca = new Palanca(mundo, 170, 120, asignarIDActor());
         PlataformaMovil plataformaMovil = new PlataformaMovil(mundo, 200, 130, 2, 200, palanca, asignarIDActor());
 
-        // Crear puerta
+        // Crear puerta y llave
         PuertaLlegada puerta = new PuertaLlegada(mundo, 590, 50, asignarIDActor());
-
-        //Crear boton
         LlaveActivadora llave = new LlaveActivadora(mundo, 532, 210, puerta, asignarIDActor());
 
         // Crear enemigo
-        Enemigo enemigo = new Enemigo(mundo, 400, 150, this, asignarIDActor());        
+        Enemigo enemigo = new Enemigo(mundo, 400, 150, this, asignarIDActor());
 
-        // Agregar todos los actores
+        // Agregar todos los actores a la escena
         super.escena.addActor(super.jugador1);
         super.escena.addActor(super.jugador2);
         super.escena.addActor(puerta);
@@ -90,10 +97,16 @@ public class Nivel1 extends NivelBase {
         for (int i = 0; i < origen.getMejorasSalto(); i++) {
             destino.mejorarSalto();
         }
+
+        // Copiar mejoras de daño
+        for (int i = 0; i < origen.getMejorasDaño(); i++) {
+            destino.mejorarDaño();
+        }
     }
 
+    // Método para asignar IDs únicos a los actores
     private int asignarIDActor() {
-    	this.cantActores++;
-    	return this.cantActores;
+        this.cantActores++;
+        return this.cantActores;
     }
 }
