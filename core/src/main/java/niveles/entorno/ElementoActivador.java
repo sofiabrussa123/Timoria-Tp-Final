6bace41e97734012db1fe0d6cde183a183cfee82
Silver_Rose <130399374+sofiabrussa123@users.noreply.github.com>
2025-12-10 20:28:47 -0300

    Update ElementoActivador.java

--- core/src/main/java/niveles/entorno/ElementoActivador.java
+++ core/src/main/java/niveles/entorno/ElementoActivador.java
@@ -5,28 +5,34 @@ import com.badlogic.gdx.graphics.g2d.Batch;
 import com.badlogic.gdx.physics.box2d.FixtureDef;
 import com.badlogic.gdx.physics.box2d.World;
 
-import Red.HiloServidor;
-import niveles.NivelBase;
+/**
+ * Clase base abstracta para elementos del entorno que pueden ser activados.
+ * Estos elementos funcionan como sensores en el mundo físico (no colisionan sólidamente).
+ */
+public abstract class ElementoActivador extends ElementoEntorno {
 
-public abstract class ElementoActivador extends ElementoEntorno{
-	protected boolean activado = false;
-	protected Texture textura;
-	protected HiloServidor hiloServidor;
-	
-	protected ElementoActivador(World mundo, float x, float y, int id) {
-		super(mundo, x, y, id);
-	}
-	
-	protected void crearYPosicionarCuerpo(float ancho, float alto) {
-		super.ancho = ancho;
-		super.alto = alto;
-		FixtureDef fixtureDef = new FixtureDef();
-        fixtureDef.isSensor = true;
+    protected boolean activado = false;
+    protected Texture textura;
+
+    // Constructor base para elementos activadores
+    protected ElementoActivador(World mundo, float x, float y, int id) {
+        super(mundo, x, y, id);
+    }
+
+    // Crea y posiciona el cuerpo físico del elemento como sensor
+    // Los sensores detectan colisiones pero no generan respuesta física
+    protected void crearYPosicionarCuerpo(float ancho, float alto) {
+        super.ancho = ancho;
+        super.alto = alto;
+        
+        FixtureDef fixtureDef = new FixtureDef();
+        fixtureDef.isSensor = true; // Configurar como sensor (sin colisión física)
+        
         super.setFixtureDef(fixtureDef);
         super.crearYPosicionarCuerpo();
-	}
-	
-	@Override
+    }
+
+    @Override
     public void draw(Batch batch, float parentAlpha) {
         batch.draw(textura, getX(), getY(), getWidth(), getHeight());
     }
