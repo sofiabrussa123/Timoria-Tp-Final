2916ca1d67eb28214efb829ab43fb8f906b2a8ca
Silver_Rose <130399374+sofiabrussa123@users.noreply.github.com>
2025-12-14 15:26:35 -0300

    Update GameController.java

--- core/src/main/java/interfaces/GameController.java
+++ core/src/main/java/interfaces/GameController.java
@@ -1,21 +1,32 @@
 package interfaces;
 
 public interface GameController {
-	void procesarAccionesEntidades(String[] mensaje);
-	void cambiarPantalla();
-	void recogerItem(int idLlave, int idJugador);
-	void dañarJugador(int idJugador, int nuevaVida);
-	void moverPlataformaMovil(int id, int posX, int posY);
-	void abrirPuerta(int id);
-	void actualizarPosicionEnemigo(int id, int posX, int posY);
-	void desaparecerEnemigo(int id);
-	void actualizarPosicionJugador(int id, int posX, int posY);
-	void matarJugador(int id);
-	void empezarJuego();
-	void conectar(int idJugador);
+    void procesarAccionesEntidades(String[] mensaje);
+    void cambiarPantalla();
+    void recogerItem(int idLlave, int idJugador);
+    void dañarJugador(int idJugador, int nuevaVida);
+    void moverPlataformaMovil(int id, float posX, float posY);
+    void abrirPuerta(int id);
+    void servidorDesconectado();
+
+    void actualizarPosicionEnemigo(int id, float posX, float posY);
+    void desaparecerEnemigo(int id);
+    void actualizarPosicionJugador(int id, float posX, float posY, boolean mirandoDerecha);
+
+    void matarJugador(int id);
+    void empezarJuego();
+    void conectar(int idJugador);
     void desconectar();
-	void volverAlMenu();
-	void terminarJuego();
-	void procesarAccionesJugador(String[] mensaje, int idJugador);
-	void procesarAccionesEnemigo(String[] mensaje, int idEnemigo);
+    void volverAlMenu();
+    void terminarJuego();
+    void procesarAccionesJugador(String[] mensaje, int idJugador);
+    void procesarAccionesEnemigo(String[] mensaje, int idEnemigo);
+
+    // Métodos existentes
+    void activarPalanca(int idPalanca);
+    void aplicarMejora(int idJugador, String tipoMejora);
+    void mostrarAtaqueEnemigo(int idEnemigo);
+
+    // ✅ NUEVO: Mostrar animación de ataque del jugador
+    void mostrarAtaqueJugador(int idJugador);
 }
